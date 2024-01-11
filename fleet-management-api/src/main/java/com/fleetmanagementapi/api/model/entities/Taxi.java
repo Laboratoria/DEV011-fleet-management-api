e8e491a7188388129aebfb6e728fbc0a37db7e44
kittyxcode <katherine.pino059@gmail.com>
2024-01-11 18:52:43 -0300

    Creacion de entiedades

---
+++ fleet-management-api/src/main/java/com/fleetmanagementapi/api/model/entities/Taxi.java
@@ -0,0 +1,29 @@
+package com.fleetmanagementapi.api.model.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="taxis")
+public class Taxi {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String plate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+}

3109343cb0bda86bef5a8ea9dd77e429175db2ee
Karina_mr <gatica265@gmail.com>
2024-01-11 19:59:40 -0600

    Configuración de algunas dependencias y conección a vercel

---
+++ dev-011-fleet-management-api/src/main/java/com/example/dev011fleetmanagementapi/FleetManagementApiRestApplication.java
@@ -0,0 +1,16 @@
+package com.example.dev011fleetmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagementApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagementApiRestApplication.class, args);
+	}
+	//System.out.print("test")
+//test
+
+
+}
---
+++ dev-011-fleet-management-api/src/test/java/com/example/dev011fleetmanagementapi/FleetManagementApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dev011fleetmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

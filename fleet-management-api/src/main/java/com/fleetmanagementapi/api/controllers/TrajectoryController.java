8c60f664f2ec47be7e1e2ad08b430edb5b3363a3
MariaPaulaArdilam <antonietainfo@gmail.com>
2024-01-22 11:58:13 -0500

    se adiciona buscar por fecha

--- fleet-management-api/src/main/java/com/fleetmanagementapi/api/controllers/TrajectoryController.java
+++ fleet-management-api/src/main/java/com/fleetmanagementapi/api/controllers/TrajectoryController.java
@@ -6,10 +6,13 @@ import com.fleetmanagementapi.api.services.ITrajectory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/v1/trajectories")
@@ -18,9 +21,9 @@ public class TrajectoryController {
     private ITrajectory iTrajectory;
 
     @GetMapping()
-    public Page<Trajectory> getTrajectoriesByTaxiId(@RequestParam Integer taxiId){
+    public Page<Trajectory> getTrajectoriesByTaxiId(@RequestParam Integer taxiId, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date date){
         PageRequest pageable = PageRequest.of(2, 2);
-        return this.iTrajectory.listTrajectoriesById(pageable, taxiId);
+        return this.iTrajectory.listTrajectoriesById(pageable, taxiId, date);
     }
 
 }

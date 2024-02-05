032c1d9f23cd15410262fc4fc7679bb46e6d2df2
kittyxcode <katherine.pino059@gmail.com>
2024-02-05 17:45:35 -0300

    Se agrega funcionalidad de verbo Post, para insertar taxis en la tabla taxi

--- fleet-management-api/src/main/java/com/fleetmanagementapi/api/services/ITaxi.java
+++ fleet-management-api/src/main/java/com/fleetmanagementapi/api/services/ITaxi.java
@@ -4,9 +4,14 @@ import com.fleetmanagementapi.api.model.entities.Taxi;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface ITaxi {
 
-    Page<Taxi> obtenerTodosLosTaxis(Pageable pageable);
+    Page<Taxi> findAllTaxis(Pageable pageable);
+
+    Taxi save (Taxi taxi);
+
+    void delete(Taxi taxi);
+
+
+
 }

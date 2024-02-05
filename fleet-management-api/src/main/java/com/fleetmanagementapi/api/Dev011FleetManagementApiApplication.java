0bc91243b7a5177ac721ee3f07a1222e147a0dae
kittyxcode <katherine.pino059@gmail.com>
2024-02-05 17:47:54 -0300

    Se agrega funcionalidad de verbo Post, para insertar taxis en la tabla taxi

--- fleet-management-api/src/main/java/com/fleetmanagementapi/api/Dev011FleetManagementApiApplication.java
+++ fleet-management-api/src/main/java/com/fleetmanagementapi/api/Dev011FleetManagementApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan({"com.fleetmanagementapi.api.services", "com.fleetmanagementapi.api.services.impl", "com.fleetmanagementapi.api.controllers"})
 public class Dev011FleetManagementApiApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(Dev011FleetManagementApiApplication.class, args);
 	}
